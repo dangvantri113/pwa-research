e81ed9a7ac15f2bb3a5c7c05f32ea5ca9cf8823c
DESKTOP-UA0R4OO\dangv <dangvantri113@gmail.com>
2020-09-06 22:21:28 +0700

    test api

--- js/app.js
+++ js/app.js
@@ -1,42 +1,56 @@
 const container = document.querySelector(".container");
-const coffees = [
-  {
-    name: "Perspiciatis",
-    image: "images/coffee1.jpg"
-  },
-  {
-    name: "Voluptatem",
-    image: "images/coffee2.jpg"
-  },
-  {
-    name: "Explicabo",
-    image: "images/coffee3.jpg"
-  },
-  {
-    name: "Rchitecto",
-    image: "images/coffee4.jpg"
-  },
-  {
-    name: " Beatae",
-    image: "images/coffee5.jpg"
-  },
-  {
-    name: " Vitae",
-    image: "images/coffee6.jpg"
-  },
-  {
-    name: "Inventore",
-    image: "images/coffee7.jpg"
-  },
-  {
-    name: "Veritatis",
-    image: "images/coffee8.jpg"
-  },
-  {
-    name: "Accusantium",
-    image: "images/coffee9.jpg"
+const NASA_KEY = 'A69yhDJ7vAsvXio2KdkhNa0Jw5CFkOpMtV7Pw2Px';
+fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer?q=How%20much%20vitamin%20c%20is%20in%202%20apples%253F", {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "x-rapidapi-key": "e33f0d152amsha7cea4c27a92895p1eff89jsn1ea37f7b44aa"
   }
-];
+})
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+// const coffees = [
+//   {
+//     name: "Perspiciatis",
+//     image: "images/coffee1.jpg"
+//   },
+//   {
+//     name: "Voluptatem",
+//     image: "images/coffee2.jpg"
+//   },
+//   {
+//     name: "Explicabo",
+//     image: "images/coffee3.jpg"
+//   },
+//   {
+//     name: "Rchitecto",
+//     image: "images/coffee4.jpg"
+//   },
+//   {
+//     name: " Beatae",
+//     image: "images/coffee5.jpg"
+//   },
+//   {
+//     name: " Vitae",
+//     image: "images/coffee6.jpg"
+//   },
+//   {
+//     name: "Inventore",
+//     image: "images/coffee7.jpg"
+//   },
+//   {
+//     name: "Veritatis",
+//     image: "images/coffee8.jpg"
+//   },
+//   {
+//     name: "Accusantium",
+//     image: "images/coffee9.jpg"
+//   }
+// ];
 const showCoffees = () => {
   let output = "";
   coffees.forEach(
