c959b1455333acaf8ab33149b756b0d581fe368d
DESKTOP-UA0R4OO\dangv <dangvantri113@gmail.com>
2020-09-06 22:58:40 +0700

    test api

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=aa5a4a416f0f4459
       console.log(response);
     })
     .then(data => {
-        console.log('Success:', data);
+        console.log(data)
     })
     .catch(err => {
       console.log(err);
